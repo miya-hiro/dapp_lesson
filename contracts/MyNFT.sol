300f5866eaade5cd0bc455a464918cb00bdf4f02
Remix Workspace <>
2022-08-12 23:13:03 +0900

    fairumei henkou

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -4,14 +4,13 @@ pragma solidity ^0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyNft is ERC721 {
+contract MyNFT is ERC721 {
 
     //init
     constructor() ERC721 ("MyNft", "MYNFT") {}
 
     function nftMint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
-        
+        _mint(to, tokenId); 
     }
 }
 
