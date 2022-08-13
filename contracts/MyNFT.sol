836037539963d359629e127caa1d93337808b288
Remix Workspace <>
2022-08-13 09:19:59 +0900

    1bu kan

--- contracts/MyNFT.sol
+++ contracts/MyNFT.sol
@@ -5,9 +5,8 @@ pragma solidity ^0.8.14;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract MyNFT is ERC721 {
-
     //init
-    constructor() ERC721 ("MyNft", "MYNFT") {}
+    constructor() ERC721 ("MyNFT", "MYNFT") {}
 
     function nftMint(address to, uint256 tokenId) public {
         _mint(to, tokenId); 
